0d6d10661dde5e74e99471e868a8833402886284
斉藤吏功 <riku@MacBook-Air.local>
2025-08-08 21:10:56 +1000

    opened from xcode

---
+++ TODO_swift/TODO_swift/TODO_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  TODO_swiftApp.swift
+//  TODO_swift
+//
+//  Created by 斉藤吏功 on 2025/08/08.
+//
+
+import SwiftUI
+
+@main
+struct TODO_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
